62aa657a00f9f82e7acc7b93f0b3668c756b5972
eduardo.schoepf <eduardo.schoepf@hotmail.com>
2022-12-28 17:29:03 +0100

    ADD find method

---
+++ findMethod.js
@@ -0,0 +1,10 @@
+/** The find() method returns  
+*** the first element in the provided array
+*** that satisfies the provided testing function
+*** #javascript #developer #js #br
+*/
+const fruits = ['🍉', '🍊', '🍎', '🍓'];
+const watermelon = fruits.find(
+  fruit => fruit === '🍉'
+);
+console.log(watermelon);
